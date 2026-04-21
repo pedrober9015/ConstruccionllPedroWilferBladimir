d29b06611c24fdc168ac4bcd71230cdbb16884ed
pedrober9015 <pedro.bermudez@correo.tdea.edu.co>
2026-04-21 17:14:40 -0500

    nuevos archivos

---
+++ cs2/src/main/java/app/Cs2Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs2Application.class, args);
+	}
+
+}
